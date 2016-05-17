67a24198af0951c2b151ab9187e5e9f59c3365e1
Theodora Chu <theodorachu@DN0a226811.SUNet>
2016-05-17 14:17:03 -0700

    Gets final image path.

--- grab_data.py
+++ grab_data.py
@@ -7,12 +7,19 @@ from cd import cd
 ck_path = os.getcwd() + '/images/CK+/cohn-kanade-plus-images/'
 with cd(ck_path):
     all_dir = os.listdir('.')
-    for directory in all_dir:
+    for directory in all_dir: #S-level
         if directory[0] == '.':
             continue
         else:
             subdir_path = ck_path + directory + '/'
             with cd(subdir_path):
-                all_images = os.listdir('.')
-                img_path = subdir_path+all_images[-1]
-                print img_path
+                all_subdirs = os.listdir('.') #0-level
+                for subdir in all_subdirs:
+                    if subdir[0] == '.':
+                        continue
+                    else:
+                        img_level_path = subdir_path + subdir + '/'
+                        with cd(img_level_path):
+                            all_imgs = os.listdir('.')
+                            last_img_path = img_level_path + all_imgs[-1]
+                            print last_img_path
